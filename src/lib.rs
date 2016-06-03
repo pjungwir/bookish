7a9a6326adedfde747bbca7ab132f14c575e0e79
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2016-06-03 07:39:38 -0700

    Don't hardcode created_at and updated_at

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(non_ascii_idents)]
 #![feature(custom_derive, custom_attribute, plugin)]
 #![plugin(diesel_codegen, dotenv_macros)]
 
@@ -8,6 +9,7 @@ pub mod models;
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
+extern crate time;
 
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
@@ -30,11 +32,19 @@ pub fn create_author<'a>(conn: &PgConnection,
                          last_name: &'a str) -> Author {
   use schema::authors;
 
+  let t = time::get_time();
+  // Postgres expects *micro*seconds.
+  // Also its Epoch starts Jan 1, 2000,
+  // but the Unix Epoch starts Jan 1, 1970.
+  let μs = 1_000_000 * t.sec + (t.nsec as i64 / 1000);
+  let μs = μs - 946684800000000;  // 2000-01-01 in μs
+  // println!("{}", μs);
+
   let a = NewAuthor {
     first_name: first_name,
     last_name: last_name,
-    created_at: PgTimestamp(500736408041057),
-    updated_at: PgTimestamp(500736408041057),
+    created_at: PgTimestamp(μs),
+    updated_at: PgTimestamp(μs),
   };
 
   diesel::insert(&a).into(authors::table)
